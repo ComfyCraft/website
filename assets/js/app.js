6054061844004c3ce33112c0ea9f09d9f391d334
seekeroftacos <theoneandonlyseek@gmail.com>
2021-05-09 14:32:18 -0500

    New homepage!

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var discordButton = document.getElementById('discordButton');
+var url = 'https://discord.gg/Ezj8mRTxFZ';
+
+discordButton.addEventListener('click', async (e) => {
+  window.open(url);
+});
